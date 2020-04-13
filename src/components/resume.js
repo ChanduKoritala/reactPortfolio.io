827dedc6600c35da41f96689182f97e1d245f156
Chandu Koritala <chanduk0406@gmail.com>
2020-04-13 19:33:08 +0930

    added div tags to aboutme and resume pages

--- src/components/resume.js
+++ src/components/resume.js
@@ -21,7 +21,9 @@ class Resume extends Component {
 
                     </Cell>
                 </Grid>
-                <FooterComp/>
+                <div>
+                    <FooterComp />
+                </div>
             </div>
         )
     }
